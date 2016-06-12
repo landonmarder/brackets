00aa26c2c93cb53cd27edec35735dc872dc0bbe2
landonmarder <landon.marder@gmail.com>
2016-06-12 17:26:32 -0400

    determine winner of contest

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -5,7 +5,13 @@ class Contest < ActiveRecord::Base
 
   validates_presence_of :round_id
 
+  delegate :expired?, to: :round
+
   def contestant_in_lead
     contest_contestants.includes(:contestant).max_by(&:votes_count).contestant
+  end
+
+  def winner
+    expired? ? contestant_in_lead : nil
   end
 end
--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -27,4 +27,34 @@ RSpec.describe Contest, type: :model do
       end
     end
   end
+
+  describe ".winner" do
+    let(:contest) { FactoryGirl.create(:contest_with_contestants) }
+    let(:round) { contest.round }
+    let(:first_contest_contestant) { contest.contest_contestants.first }
+    let(:second_contest_contestant) { contest.contest_contestants.last }
+    let!(:first_vote) { FactoryGirl.create(:vote, contest_contestant: first_contest_contestant) }
+    let!(:second_vote) { FactoryGirl.create(:vote, contest_contestant: second_contest_contestant) }
+    let!(:third_vote) { FactoryGirl.create(:vote, contest_contestant: second_contest_contestant) }
+
+    context "contest has expired" do
+      before do
+        round.expires_at = 1.year.ago
+      end
+
+      it "should return the contestant in the lead" do
+        expect(contest.winner).to eq(second_contest_contestant.contestant)
+      end
+    end
+
+    context "contest has not expired" do
+      before do
+        round.expires_at = 1.year.from_now
+      end
+
+      it "should return nil" do
+        expect(contest.winner).to be_nil
+      end
+    end
+  end
 end
