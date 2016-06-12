234c37a5720293879bacafc7575667d6808a859e
landonmarder <landon.marder@gmail.com>
2016-06-12 17:32:38 -0400

    add uniqueness validation for ip address on votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :contest_contestant, dependent: :destroy, counter_cache: true
 
   validates_presence_of :contest_contestant_id
+  validates_uniqueness_of :ip_address, allow_nil: true
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
+  subject { Vote.new(contest_contestant_id: 1) }
   it { should validate_presence_of(:contest_contestant_id) }
+  it { should validate_uniqueness_of(:ip_address).allow_nil }
   it { should belong_to(:contest_contestant).counter_cache(true).dependent(:destroy) }
 end
